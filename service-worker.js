a95dc6d028a52de18cfa7c170f33362623e8c365
Tayyab-Ijaz <tayyabijaz384@gmail.com>
2026-09-21 21:08:47 +0500

    restyled commit

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 //
 // Bump CACHE_VERSION whenever any precached file changes -- otherwise installed copies keep
 // serving the old files. (webapp/parity/pwa_offline_test.mjs fails if a precached file is missing.)
-const CACHE_VERSION = 'ppd-adapt-v9';
+const CACHE_VERSION = 'ppd-adapt-v10';
 const PRECACHE_URLS = [
   './',
   './index.html',
